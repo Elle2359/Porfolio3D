0647b0a44e512a7e618993853a75ef02cb405d06
Elle'Vera <lyod.knyazh@gmail.com>
2024-02-20 14:19:48 +0530

    commit 1

---
+++ app/config.js
@@ -0,0 +1,9 @@
+export const config = {
+  name: 'Maitree Madhuri',
+  role: 'Software',
+  disciplines: ['Engineer', 'Developer', 'Data science', '', 'Data analyst'],
+  url: 'https://hamishw.com',
+  twitter: '@ElleVera10',
+  figma: 'profile/lyod_knyazh',
+  github: 'Elle2359',
+};
