ec40db633843c03f42d9e60746d05332221fba42
Elle'Vera <81345563+Elle2359@users.noreply.github.com>
2024-02-25 15:47:24 +0530

    Update config.js

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@ export const config = {
   name: 'Maitree Madhuri',
   role: 'Software',
   disciplines: ['Engineer', 'Developer', 'Data science', '', 'Data analyst'],
-  url: 'https://hamishw.com',
+  url: 'https://7589eae6.porfolio3d.pages.dev/',
   twitter: '@ElleVera10',
   figma: 'profile/lyod_knyazh',
   github: 'Elle2359',
